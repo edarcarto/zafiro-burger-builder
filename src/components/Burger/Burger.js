5c08545796285518bf54f0d1fb9adae1a27fb5fe
Efren Carrillo <efren.sistemas@gmail.com>
2019-02-19 11:17:28 -0500

    [Fix] Ingredientes dinámicos

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,18 @@ import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredients/BurgerIngredient';
 
 const burger = (props) => {
+    //Object.keys => transforma un objeto en array
+    const transformIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            //tranformar el contenido (valor numerico) en array
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />
+            });
+        });
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
